443e0178f961e73470e068b616ff0dedfd781f21
manitu85 <manitu.psy@gmail.com>
2019-12-28 01:11:43 +0100

    #

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import Aux  from '../../hoc/Aux'
+import Wrapper  from '../../hoc/Wrapper'
 import classes from './Layout.module.scss'
 
 
 const Layout = (props) => (
-  <Aux>
+  <Wrapper>
     <div>
       Toolbar, SideDrawer, Backdrop      
     </div>
     <main className={classes.Content} >
       {props.children}
     </main>
-  </Aux>
+  </Wrapper>
 )
 
 export default Layout
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Wrapper from '../../hoc/Wrapper'
+
+class BurgerBuilder extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Wrapper>
+    )
+  }
+}
+
+export default BurgerBuilder
