2dc28c1bc072ac3aaca29d945cc610bc16c40b48
manitu85 <manitu.psy@gmail.com>
2020-01-18 17:20:46 +0100

    #

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -15,8 +15,8 @@ const initialState = {
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
-  meat: 1.3,
-  bacon: 0.7
+  meat: 1,
+  bacon: 0.5
 }
 
 const addIngredient = (state, action) => {
