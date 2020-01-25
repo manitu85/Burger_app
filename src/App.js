80cb3e7cdfbbdf0c1a842cb9abb3f9d1dcd9d6c1
manitu85 <manitu.psy@gmail.com>
2020-01-25 19:24:14 +0100

    #

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Logout from './containers/Auth/Logout/Logout'
 import * as actions from './store/actions/index'
 
 class App extends Component {
-
+  
   componentDidMount() {
     this.props.onTryAutoSignup()
   }
@@ -18,6 +18,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
+        <Route path="/auth" component={Auth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -29,7 +30,6 @@ class App extends Component {
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch>
